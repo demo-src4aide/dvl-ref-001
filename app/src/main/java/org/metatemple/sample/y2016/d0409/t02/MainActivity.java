a6be3bce6cc3751583d112ba24239b7f78751e82
Sophia Shapira <Sophia.Shapira@gmail.com>
2016-04-09 23:19:53 -0400

    I created a button that results in exiting the app.

--- app/src/main/java/org/metatemple/sample/y2016/d0409/t02/MainActivity.java
+++ app/src/main/java/org/metatemple/sample/y2016/d0409/t02/MainActivity.java
@@ -2,6 +2,8 @@ package org.metatemple.sample.y2016.d0409.t02;
 
 import android.app.*;
 import android.os.*;
+import android.widget.*;
+import android.view.*;
 
 public class MainActivity extends Activity 
 {
@@ -11,4 +13,16 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+	
+	// The following function is referenced
+	// by a button's 'android:onClick' attribute
+	// in the 'main.xml' file.
+	public void do_for_okay (View view)
+	{
+		// This button, of course, does nothing
+		// but exit the application.
+		finish();
+	}
+	
 }
+
